363419c6f3b5ba0516a8cfce83a552e92ccb2351
Kuzmin Max <different@mail.ru>
2024-12-12 11:21:01 +0300

    first commit

---
+++ src/Manager.java
@@ -0,0 +1,13 @@
+public class Manager extends Profession {
+
+    @Override
+    public String getName() {
+        return "Менеджер";
+    }
+
+    @Override
+    public int getLunchDuration() {
+        return 60;
+    }
+
+}
